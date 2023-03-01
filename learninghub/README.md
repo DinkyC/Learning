30c608fd179b9f2ea406692e26e6bfc247a6e0e5
DinkyC <kb1ubp@gmail.com>
2023-03-01 01:45:39 -0800

    Let it begin

---
+++ learninghub/README.md
@@ -0,0 +1,6 @@
+# Hi statistics lovers!
+
+I have made a function for standard deviation. Take a look at the functions as it takes you step by step. 
+
+I will be adding more comments to the code shortly
+
---
+++ learninghub/standardev.py
@@ -0,0 +1,25 @@
+import math
+
+def standard_dev(lst_2) -> None:
+    dev = deviations(mean(lst_2), lst_2)
+    sv = samplevar(dev)
+    stdv = round(sv_square(sv, lst_2), 3)
+    return stdv
+
+def mean(lst_2):
+    mn = sum(lst_2) / len(lst_2)
+    return mn
+
+def deviations(mn, lst_2):
+    dev = [l - mn for l in lst_2]
+    return dev
+
+def samplevar(dev):
+    dev_squared = [d ** 2 for d in dev]
+    total = sum(dev_squared)
+    sv = round((total / (len(dev_squared))), 3)
+    return sv
+
+def sv_square(sv, lst_2):
+    sqr = math.sqrt(sv)
+    return sqr
