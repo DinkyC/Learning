e2cdf3c2ba4caeb22a1b31f5dff7649b47ad0a81
DinkyC <kb1ubp@gmail.com>
2023-03-01 04:49:38 -0800

    new

--- Statistics/standard_dev.py
+++ Statistics/standard_dev.py
@@ -1,13 +1,13 @@
 import math
 
-def standard_dev(lst_2) -> None:
+def standard_dev(lst_2):
     dev = deviations(mean(lst_2), lst_2)
     sv = samplevar(dev)
     stdv = round(sv_square(sv, lst_2), 3)
     return stdv
 
 def mean(lst_2):
-    mn = sum(lst_2) / len(lst_2)
+    mn = math.fsum(lst_2) / (len(lst_2))
     return mn
 
 def deviations(mn, lst_2):
@@ -16,8 +16,8 @@ def deviations(mn, lst_2):
 
 def samplevar(dev):
     dev_squared = [d ** 2 for d in dev]
-    total = sum(dev_squared)
-    sv = round((total / (len(dev_squared))), 3)
+    total = math.fsum(dev_squared)
+    sv = (total / (len(dev_squared) - 1))
     return sv
 
 def sv_square(sv, lst_2):
